38e9a7da7f06faa6b5b94f0ea4ea54203d560dac
CrispenGari <Crispengari@gmail.com>
2021-06-16 20:07:30 +0200

    nav-bar-header-top-responsive

--- youtube/components/Head/Head.tsx
+++ youtube/components/Head/Head.tsx
@@ -2,7 +2,7 @@ import NextHead from "next/head";
 const Head = () => (
   <NextHead>
     <meta charSet="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link
       rel="shortcut icon"
