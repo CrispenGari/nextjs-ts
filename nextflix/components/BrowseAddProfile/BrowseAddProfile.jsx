965ffbb61a3057e917048ee8259026899da0c3e0
CrispenGari <Crispengari@gmail.com>
2021-06-20 16:27:40 +0200

    netflix-add-ui-done

---
+++ nextflix/components/BrowseAddProfile/BrowseAddProfile.jsx
@@ -0,0 +1,24 @@
+import styles from "./BrowseAddProfile.module.css";
+
+const BrowseAddProfile = () => {
+  return (
+    <div className={styles.browse__add__profile}>
+      <h1>Browse Add</h1>
+      <p>Add a profile for another person watching Netflix.</p>
+      <div className={styles.browse__add__profile__center}>
+        <img src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/366be133850498.56ba69ac36858.png" />
+        <input type="text" placeholder="Name" />
+        <label htmlFor="is-kid-check">
+          <input type="checkbox" id="is-kid-check" />
+          Kid?
+        </label>
+      </div>
+      <div className={styles.browse__add__profile__buttons}>
+        <button>Continue</button>
+        <button>Cancel</button>
+      </div>
+    </div>
+  );
+};
+
+export default BrowseAddProfile;
--- nextflix/pages/browse.js
+++ nextflix/pages/browse.js
@@ -2,15 +2,18 @@ import styles from "../styles/Browse.module.css";
 
 import BrowseHeader from "../components/BrowseHeader/BrowseHeader";
 import BrowseColumn from "../components/BrowseColumn/BrowseColumn";
+import BrowseAddProfile from "../components/BrowseAddProfile/BrowseAddProfile";
 
 const Browse = () => {
   return (
     <div className={styles.browse}>
       <BrowseHeader />
+
       <div className={styles.browse__main}>
-        <h1>Who's watching?</h1>
+        <BrowseAddProfile />
+        {/* <h1>Who's watching?</h1>
         <BrowseColumn />
-        <button>Manage Profiles</button>
+        <button>Manage Profiles</button> */}
       </div>
     </div>
   );
