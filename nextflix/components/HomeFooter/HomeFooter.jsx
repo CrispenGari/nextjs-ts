3d6380121524e3e0bd15288c61ddc6b67cc8fdd1
CrispenGari <Crispengari@gmail.com>
2021-06-21 18:58:23 +0200

    home-footer-ui-done

--- nextflix/components/HomeFooter/HomeFooter.jsx
+++ nextflix/components/HomeFooter/HomeFooter.jsx
@@ -1,9 +1,34 @@
 import styles from "./HomeFooter.module.css";
-
+import { AiFillFacebook } from "react-icons/ai";
+import { ImInstagram } from "react-icons/im";
+import { GrYoutube } from "react-icons/gr";
 const HomeFooter = () => {
   return (
     <div className={styles.home__footer}>
-      <h1>Home Footer</h1>
+      <div className={styles.home__footer__main}>
+        <div>
+          <AiFillFacebook className={styles.home__footer__social__button} />
+          <ImInstagram className={styles.home__footer__social__button} />
+          <GrYoutube className={styles.home__footer__social__button} />
+        </div>
+        <div className={styles.home__footer__main__links}>
+          <p>Audio and Subtitles</p>
+          <p>Help Center</p>
+          <p>Media Center</p>
+          <p>Jobs</p>
+          <p>Privacy</p>
+          <p>Cookie Preferences</p>
+          <p>Contact Us</p>
+          <p>Audio Description</p>
+          <p>Gift Cards</p>
+          <p>Investor Relations</p>
+          <p>Terms of use</p>
+          <p>Legal Notices</p>
+          <p>Corporate Information</p>
+        </div>
+        <button>Service Code</button>
+        <p>1997-2021 Netflix, Inc. [1b1ksosuiqsoagahejaiospjaiu67290]</p>
+      </div>
     </div>
   );
 };
--- nextflix/pages/index.js
+++ nextflix/pages/index.js
@@ -1,9 +1,15 @@
 import styles from "../styles/Home.module.css";
 
 import HomeHeader from "../components/HomeHeader/HomeHeader";
+import HomeFooter from "../components/HomeFooter/HomeFooter";
+import HomeBarner from "../components/HomeBarner/HomeBarner";
+import HomeRows from "../components/HomeRows/HomeRows";
 const Home = () => (
   <div className={styles.home}>
     <HomeHeader />
+    <HomeBarner />
+    <HomeRows />
+    <HomeFooter />
   </div>
 );
 
