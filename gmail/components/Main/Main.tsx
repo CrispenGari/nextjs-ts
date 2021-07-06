2599b9dec47c26eae291b952ed99541b58da7459
CrispenGari <Crispengari@gmail.com>
2021-07-06 20:30:26 +0200

    main-header

--- gmail/components/Main/Main.tsx
+++ gmail/components/Main/Main.tsx
@@ -1,7 +1,12 @@
 import styles from "./Main.module.css";
 
+import MainHeader from "../MainHeader/MainHeader";
 const Main = () => {
-  return <div className={styles.main}></div>;
+  return (
+    <div className={styles.main}>
+      <MainHeader />
+    </div>
+  );
 };
 
 export default Main;
--- gmail/components/MainHeader/MainHeader.tsx
+++ gmail/components/MainHeader/MainHeader.tsx
@@ -1,9 +1,29 @@
 import styles from "./MainHeader.module.css";
 
+import { BiCheckbox } from "react-icons/bi";
+import { IoRefresh } from "react-icons/io5";
+import { MdMoreVert } from "react-icons/md";
+import { BiCaretDown } from "react-icons/bi";
+
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 const MainHeader = () => {
   return (
     <div className={styles.main__header}>
-      <h1>Main Header</h1>
+      <div className={styles.main__header__left}>
+        <div title="Select">
+          <BiCheckbox className={styles.main__header__icon} />
+          <BiCaretDown className={styles.main__header__icon} />
+        </div>
+        <IoRefresh title="Refresh" className={styles.main__header__icon} />
+        <MdMoreVert title="More" className={styles.main__header__icon} />
+      </div>
+      <div className={styles.main__header__right}>
+        <div>1-50 of 147</div>
+        <div>
+          <FiChevronLeft title="Newer" className={styles.main__header__icon} />
+          <FiChevronRight title="Older" className={styles.main__header__icon} />
+        </div>
+      </div>
     </div>
   );
 };
