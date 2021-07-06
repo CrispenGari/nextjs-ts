0e48c0e6d00f7ed49447257671d9abb234673de0
CrispenGari <Crispengari@gmail.com>
2021-07-06 19:57:25 +0200

    shortcuts-ui-done

--- gmail/components/Shortcuts/Shortcuts.tsx
+++ gmail/components/Shortcuts/Shortcuts.tsx
@@ -1,9 +1,31 @@
 import styles from "./Shortcuts.module.css";
 
+import { IconButton } from "@material-ui/core";
+import { BsCalendar } from "react-icons/bs";
+import { FcCalendar } from "react-icons/fc";
+import { VscLightbulb } from "react-icons/vsc";
+import { CgGoogleTasks } from "react-icons/cg";
+import { IoPersonCircle } from "react-icons/io5";
+import { IoIosAdd } from "react-icons/io";
 const Shortcuts = () => {
   return (
     <div className={styles.shortcuts}>
-      <h1>Shorcts</h1>
+      <IconButton title="Calender">
+        <FcCalendar />
+      </IconButton>
+      <IconButton title="Keep">
+        <VscLightbulb />
+      </IconButton>
+      <IconButton title="Tasks">
+        <CgGoogleTasks />
+      </IconButton>
+      <IconButton title="Contacts">
+        <IoPersonCircle />
+      </IconButton>
+      <div />
+      <IconButton title="Get adds-ons">
+        <IoIosAdd />
+      </IconButton>
     </div>
   );
 };
